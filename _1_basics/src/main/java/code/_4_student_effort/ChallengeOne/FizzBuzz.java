e8880f5bcc8b9470b10f3433fa598c647e4c509a
George <george.fuior@icloud.com>
2020-11-18 20:19:12 +0200

    Challenge One first Stage printout

--- _1_basics/src/main/java/code/_4_student_effort/ChallengeOne/FizzBuzz.java
+++ _1_basics/src/main/java/code/_4_student_effort/ChallengeOne/FizzBuzz.java
@@ -16,4 +16,12 @@ public class FizzBuzz {
         return "";
     }
 
+    public static void main(String[] args) {
+        System.out.println("Stage 1: ");
+        for(int i = 1 ; i <=100; i ++ ){
+            System.out.print(fizzBuzz(i)+",");
+        }
+    }
+
+
 }
