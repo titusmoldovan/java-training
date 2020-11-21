0d6d32f7cc834399792bfd4d791053180295ce0a
George <george.fuior@icloud.com>
2020-11-21 22:35:41 +0200

    Challenge 3

---
+++ _1_basics/src/main/java/code/_4_student_effort/ChallengeThree/PairOf2.java
@@ -0,0 +1,39 @@
+package code._4_student_effort.ChallengeThree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PairOf2 {
+
+    public static final Integer countPairs(Integer[] values){
+        int noPairs = 0;
+        //make a map with format: value = no of occurences in integer array
+        Map<Integer,Integer> occurences = new HashMap<>();
+        for (int i = 0; i < values.length; i++){
+            if(!occurences.containsKey(values[i])){
+                occurences.put(values[i],1);
+            }else{
+                occurences.put(values[i],occurences.get(values[i])+1);
+            }
+        }
+        //iterate the map
+        for (Map.Entry<Integer,Integer> entry : occurences.entrySet()){
+            //if the map contains a entry which is complement of the current entry
+            if (occurences.containsKey(0 - entry.getKey()) && entry.getKey()!=0){
+                //increment the no of pairs with the minimum value of values of the two entries
+                    noPairs+=Math.min(entry.getValue(),occurences.get(0-entry.getKey()));
+                    //set the value if complement of the current entry with 0 to not make additional increment
+                    //when the loop will fint the complement
+                    occurences.put(0-entry.getKey(),0);
+            }
+        }
+
+        return noPairs;
+    }
+
+    public static void main(String[] args) {
+        Integer[] sir = new Integer[]{1, 1, 0, -1, -1};
+        countPairs(sir);
+
+    }
+}
--- _1_basics/src/test/java/code/_2_challenge/_3_pair_of_2/PairOf2Test.java
+++ _1_basics/src/test/java/code/_2_challenge/_3_pair_of_2/PairOf2Test.java
@@ -1,5 +1,6 @@
 package code._2_challenge._3_pair_of_2;
 
+import code._4_student_effort.ChallengeThree.PairOf2;
 import org.junit.Assert;
 
 import java.util.Arrays;
@@ -11,12 +12,14 @@ public class PairOf2Test {
   public static Map<Integer[], Integer> data = Map.ofEntries(
           entry(new Integer[]{3, 2, -3, -2, 3, 0}, 2),
           entry(new Integer[]{1, 1, 0, -1, -1}, 2),
-          entry(new Integer[]{5, 9, -5, 7, -5}, 1)
+          entry(new Integer[]{5, 9, -5, 7, -5}, 1),
+          entry(new Integer[]{0, -5, 1, 3, -1, 5, 2,-5,-5},2),
+          entry(new Integer[]{5,-5,5,9,0,9,2},1)
   );
 
   @org.junit.Test
   public void test() {
-    PairOf2 PairOf2 = new PairOf2();
+    code._4_student_effort.ChallengeThree.PairOf2 PairOf2 = new PairOf2();
     for (Map.Entry<Integer[], Integer> entry : data.entrySet()) {
       Integer[] input = entry.getKey();
       Integer expected = entry.getValue();
