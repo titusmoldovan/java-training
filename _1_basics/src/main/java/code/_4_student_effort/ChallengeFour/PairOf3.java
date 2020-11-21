9974d160ccc48cddb65fa85327df6117ae1fcad5
George <george.fuior@icloud.com>
2020-11-21 23:32:26 +0200

    Challenge 4

---
+++ _1_basics/src/main/java/code/_4_student_effort/ChallengeFour/PairOf3.java
@@ -0,0 +1,26 @@
+package code._4_student_effort.ChallengeFour;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PairOf3 {
+    public static final int countPairs(Integer[] arr, int sum){
+        int noPairs = 0;
+        Map<Integer,Boolean> isInPair = new HashMap<Integer,Boolean>();
+        for(int i = 0 ; i < arr.length ; i ++){
+            for( int j = i+1; j < arr.length ; j++){
+                for( int k = j+1; k < arr.length; k ++){
+                    if(!isInPair.containsKey(i) && !isInPair.containsKey(j) && !isInPair.containsKey(k)) {
+                        if (arr[i] + arr[j] + arr[k] == sum) {
+                            noPairs += 1;
+                            isInPair.put(i, true);
+                            isInPair.put(j, true);
+                            isInPair.put(k, true);
+                        }
+                    }
+                }
+            }
+        }
+        return noPairs;
+    }
+}
--- _1_basics/src/test/java/code/_2_challenge/_4_pair_of_3/PairOf3Test.java
+++ _1_basics/src/test/java/code/_2_challenge/_4_pair_of_3/PairOf3Test.java
@@ -1,5 +1,6 @@
 package code._2_challenge._4_pair_of_3;
 
+import code._4_student_effort.ChallengeFour.PairOf3;
 import org.junit.Assert;
 
 import java.util.Arrays;
@@ -9,12 +10,16 @@ import static java.util.Map.entry;
 
 public class PairOf3Test {
   public static Map<Integer[], Integer> data = Map.ofEntries(
-          entry(new Integer[]{-1, -1, -1, 2}, 1)
+          entry(new Integer[]{-1, -1, -1, 2}, 1),
+          entry(new Integer[]{2,3,-5,1,2,-3},2),
+          entry(new Integer[]{9,1,-4,-5,3,-4},2),
+          entry(new Integer[]{8,3,0,-3,2,-10},2),
+          entry(new Integer[]{1,2,1,3,-3,0},1)
   );
 
   @org.junit.Test
   public void test() {
-    PairOf3 PairOf3 = new PairOf3();
+    code._4_student_effort.ChallengeFour.PairOf3 PairOf3 = new PairOf3();
     for (Map.Entry<Integer[], Integer> entry : data.entrySet()) {
       Integer[] input = entry.getKey();
       Integer expected = entry.getValue();
