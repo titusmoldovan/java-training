d199664193ccf46810a49f5be6e79a2ef266dd5a
Maria <mciuculan@gmail.com>
2021-05-16 12:40:59 +0300

    First week

---
+++ _1_basics/src/main/java/code/_4_student_effort/Challenge2.java
@@ -0,0 +1,102 @@
+package code._4_student_effort;
+
+public class Challenge2 {
+
+    static String setDigit(int number) {
+        if (number == 3) {
+            return "Foo";
+        }
+        if (number == 5) {
+            return "Bar";
+        }
+        if (number == 7) {
+            return "Qix";
+        }
+        return "";
+    }
+
+    static String compute(int number) {
+        String modified = "";
+        boolean isOK = false;
+        int copy = number, reversed = 0;
+        for( ; copy != 0; copy /= 10) {
+            reversed = reversed * 10 + copy % 10;
+            if (copy % 10 == 3 || copy % 10 == 5 || copy % 10 == 7) {
+                isOK = true;
+            }
+        }
+        if (number % 3 == 0) {
+            modified = modified.concat("Foo");
+            isOK = true;
+        }
+        if (number % 5 == 0) {
+            modified = modified.concat("Bar");
+            isOK = true;
+        }
+        if (number % 7 == 0) {
+            modified = modified.concat("Qix");
+            isOK = true;
+        }
+        if (isOK == true) {
+            for (; reversed != 0; reversed /= 10) {
+                modified = modified.concat(setDigit(reversed % 10));
+            }
+        } else {
+            modified = modified.concat(Integer.toString(number));
+        }
+        return modified;
+    }
+
+    static String compute2(int number) {
+        String modified = "";
+        boolean isOK = false, hasZero = false;
+        int copy = number, reversed = 0;
+        for( ; copy != 0; copy /= 10) {
+            reversed = reversed * 10 + copy % 10;
+            if (copy % 10 == 3 || copy % 10 == 5 || copy % 10 == 7) {
+                isOK = true;
+            }
+            if (copy % 10 == 0) {
+                hasZero = true;
+            }
+        }
+        if (number % 3 == 0) {
+            modified = modified.concat("Foo");
+            isOK = true;
+        }
+        if (number % 5 == 0) {
+            modified = modified.concat("Bar");
+            isOK = true;
+        }
+        if (number % 7 == 0) {
+            modified = modified.concat("Qix");
+            isOK = true;
+        }
+        if (hasZero == false) {
+            modified = modified.concat(compute(number));
+        } else {
+            if (isOK == false) {
+                for( ; reversed != 0; reversed /= 10) {
+                    if (reversed % 10 == 0) {
+                        modified = modified.concat("*");
+                    } else {
+                        modified = modified.concat(Integer.toString(reversed % 10));
+                    }
+                }
+            } else {
+                for( ; reversed != 0; reversed /= 10) {
+                    if (reversed % 10 == 0) {
+                        modified = modified.concat("*");
+                    } else {
+                        modified = modified.concat(setDigit(reversed % 10));
+                    }
+                }
+            }
+        }
+        return modified;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compute2(303));
+    }
+}
---
+++ _1_basics/src/main/java/code/_4_student_effort/Challenge3.java
@@ -0,0 +1,76 @@
+package code._4_student_effort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Challenge3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        int n, noPairs = 0;
+        MyScanner sc = new MyScanner();
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            array.add(sc.nextInt());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (array.get(i) + array.get(j) == 0
+                        && array.get(i) != 0
+                        && array.get(j) != 0) {
+                    array.set(i, 0);
+                    array.set(j, 0);
+                    noPairs++;
+                }
+            }
+        }
+        System.out.println(noPairs);
+    }
+
+    static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+
