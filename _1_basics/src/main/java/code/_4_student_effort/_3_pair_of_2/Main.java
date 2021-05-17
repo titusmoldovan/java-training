5ee58a8e9af606705bcc324eedec4ffc2429244c
danciujoelle <danciujoelle@yahoo.com>
2021-05-17 12:35:26 +0300

    challenges 1,2,3,4 all done

--- _1_basics/src/main/java/code/_4_student_effort/_3_pair_of_2/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/_3_pair_of_2/Main.java
@@ -1,10 +1,11 @@
 package code._4_student_effort._3_pair_of_2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array1={3,2,-3,-2,3,0};
+       int[] array1={3,2,-3,-2,3,0};
         int[] array2={1,1,0,-1,-1};
         int[] array3={5,9,-5,7,-5};
         System.out.println(pairof2(array1));
@@ -14,20 +15,23 @@ public class Main {
     }
     public static int pairof2(int[] array){
         int pairs=0;
+        Arrays.sort(array);
         HashMap<Integer,String> map=new HashMap<Integer, String>();
         for(int i=0;i<array.length;i++){
             map.put(array[i],"no pair");
         }
         for(int i=0;i<array.length-1;i++){
-            for(int j=i+1;j<array.length;j++){
-                if(array[i]+array[j]==0&&map.get(array[i])=="no pair"){
+            for(int j=i+1;j<array.length-1;j++){
+                if(array[i]+array[j]==0&&map.get(array[i])=="no pair"&&map.get(array[j])=="no pair"){
                     pairs++;
-                    map.put(array[j],"yes");
+                    if(array[i]!=array[i+1])
                     map.put(array[i],"yes");
+                    if(array[j]!=array[j+1])
+                    map.put(array[j],"yes");
+                    break;
                 }
             }
         }
         return pairs;
-        // da rateuri daca sunt mai multe perechi de dubluri 
     }
 }
