552855e7b4c6ba5c7a8d422903e46f62e1b43f59
fane5115 <fanef92@gmail.com>
2021-07-01 11:48:52 +0300

    Project

--- _3_design_patterns/src/main/java/code/_4_student_effort/Main.java
+++ _3_design_patterns/src/main/java/code/_4_student_effort/Main.java
@@ -1,8 +1,19 @@
 package code._4_student_effort;
 
+import code._2_challenge._1_iterator.ArrayCustomIterator;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-  public static void main(String[] args) {
-    //TODO put your code changes in here
-  }
+    public static void main(String[] args) {
+        Window.getInstance();
+        int[] arr = new int[]{1, 2, 3};
+        MyIteratorImpl it = new MyIteratorImpl(arr);
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
 }
---
+++ _3_design_patterns/src/main/java/code/_4_student_effort/MyIterator.java
@@ -0,0 +1,8 @@
+package code._4_student_effort;
+
+import javax.lang.model.element.Element;
+
+public interface MyIterator {
+    public boolean hasNext();
+    public int next();
+}
