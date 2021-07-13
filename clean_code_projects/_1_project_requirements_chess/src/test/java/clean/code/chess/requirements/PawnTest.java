d828107f7135733271837f2aecebcd96ef1c8fae
Daria <dariaflorea49@gmail.com>
2021-07-13 16:45:29 +0300

    proiect 2

--- clean_code_projects/_1_project_requirements_chess/src/test/java/clean/code/chess/requirements/PawnTest.java
+++ clean_code_projects/_1_project_requirements_chess/src/test/java/clean/code/chess/requirements/PawnTest.java
@@ -8,12 +8,17 @@ import static org.junit.Assert.*;
 public class PawnTest {
 
     private ChessBoard chessBoard;
-    private Pawn testSubject;
+    private Pawn testSubject, white, black;
 
     @Before
     public void setUp() {
         this.chessBoard = new ChessBoard();
         this.testSubject = new Pawn(PieceColor.BLACK);
+        this.white = new Pawn(PieceColor.WHITE);
+        this.black = new Pawn(PieceColor.BLACK);
+        testSubject.setChessBoard(chessBoard);
+        white.setChessBoard(chessBoard);
+        black.setChessBoard(chessBoard);
     }
 
     @Test
@@ -51,6 +56,36 @@ public class PawnTest {
         testSubject.Move(MovementType.MOVE, 6, 2);
         assertEquals(6, testSubject.getXCoordinate());
         assertEquals(2, testSubject.getYCoordinate());
+    }
+
+    @Test
+    public void testPawn_Capture_LegalCoordinates_Left_UpdatesCoordinates() {
+        chessBoard.Add(black, 3,3,PieceColor.BLACK);
+        chessBoard.Add(white,4,2,PieceColor.WHITE);
+        white.Move(MovementType.CAPTURE,3,3);
+
+        assertEquals(3,white.getXCoordinate());
+        assertEquals(3,white.getYCoordinate());
+    }
+
+    @Test
+    public void testPawn_Capture_LegalCoordinates_Right_UpdatesCoordinates() {
+        chessBoard.Add(black, 3,3,PieceColor.BLACK);
+        chessBoard.Add(white,2,2,PieceColor.WHITE);
+        white.Move(MovementType.CAPTURE,3,3);
+
+        assertEquals(3,white.getXCoordinate());
+        assertEquals(3,white.getYCoordinate());
+    }
+
+    @Test
+    public void testPawn_Capture_ILegalCoordinates_UpdatesCoordinates() {
+        chessBoard.Add(black, 3,3,PieceColor.BLACK);
+        chessBoard.Add(white,3,4,PieceColor.WHITE);
+        black.Move(MovementType.CAPTURE,3,4);
+
+        assertEquals(3,black.getXCoordinate());
+        assertEquals(3,black.getYCoordinate());
     }
 
 }
